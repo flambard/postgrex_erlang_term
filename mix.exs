6773d44cbf78945a688d49495f3b68cceb3082b2
Markus Flambard <markus.flambard@youcal.com>
2022-11-08 00:25:25 +0100

    Add LICENSE and project info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule PostgrexErlangTerm.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/flambard/postgrex_erlang_term"
     ]
   end
 
@@ -22,6 +25,20 @@ defmodule PostgrexErlangTerm.MixProject do
       {:postgrex, "~> 0.16.5"},
       {:ecto, "~> 3.9"},
       {:ecto_sql, "~> 3.6"}
+    ]
+  end
+
+  defp description do
+    "PostgrexErlangTerm extends Postgrex with a type for storing Erlang terms directly in PostgreSQL. Requires the PostgreSQL extension pg_erlang_term."
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/flambard/postgrex_erlang_term",
+        "pg_erlang_term" => "https://github.com/flambard/pg_erlang_term"
+      }
     ]
   end
 end
